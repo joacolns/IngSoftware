dc0e578dec4345d4d734ca5d07198be1d523b6dc
Admin <user@172-2-212-228.lightspeed.cicril.sbcglobal.net>
2026-05-14 20:24:17 -0300

    base para el composite

---
+++ BE/BE_Componente.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BE
+{
+    public abstract class BE_Componente
+    {
+		private int id_Componente;
+
+		public int ID_Componente
+		{
+			get { return id_Componente; }
+			set { id_Componente = value; }
+		}
+
+		private string nombre;
+
+		public string Nombre
+		{
+			get { return nombre; }
+			set { nombre = value; }
+		}
+
+        public abstract void Agregar(BE_Componente permiso);
+        public abstract void Quitar(BE_Componente permiso);
+        public abstract List<BE_Componente> ObtenerHijos();
+    }
+}
---
+++ BE/BE_Hoja.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BE
+{
+    public class BE_Hoja : BE_Componente
+    {
+        // Permiso simple
+
+    }
+}
